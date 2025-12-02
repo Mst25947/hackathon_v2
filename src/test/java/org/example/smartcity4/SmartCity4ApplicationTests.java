6298b385b02334f6963c3b1faae3237922fdaab6
MaxxieMax1 <max.breunesse@gmail.com>
2025-12-02 11:50:25 +0100

    Git init

---
+++ src/test/java/org/example/smartcity4/SmartCity4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartcity4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCity4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
